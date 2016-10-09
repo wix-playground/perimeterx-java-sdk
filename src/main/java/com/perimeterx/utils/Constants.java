17b781fbfd2526c31258a2fbfa5fca22382d85b7
Juozas Salna <salna@wix.com>
2016-10-09 16:41:14 +0300

    jackson downgrade

--- src/main/java/com/perimeterx/utils/Constants.java
+++ src/main/java/com/perimeterx/utils/Constants.java
@@ -8,7 +8,7 @@ package com.perimeterx.utils;
 public final class Constants {
 
     // This token is replaced by maven on validation stage to the pom version
-    public final static String SDK_VERSION = "@moduleVersion@";
+    public final static String SDK_VERSION = "1.0.15-wix";
 
     public final static String ACTIVITY_BLOCKED = "block";
     public final static String ACTIVITY_PAGE_REQUESTED = "page_requested";
